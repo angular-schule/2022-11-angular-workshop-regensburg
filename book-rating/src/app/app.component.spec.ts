a953d44117fb7300a4cfd3832d56ba9e3f805f64
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-11-09 15:13:29 +0100

    Unit-Testing: Shallow Component Test

--- book-rating/src/app/app.component.spec.ts
+++ book-rating/src/app/app.component.spec.ts
@@ -2,6 +2,8 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { BookComponent } from './books/book/book.component';
+import { DashboardComponent } from './books/dashboard/dashboard.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -10,8 +12,11 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
-      ]
+        AppComponent,
+        // DashboardComponent,  // Integration-Test
+        // BookComponent
+      ],
+      schemas: [NO_ERRORS_SCHEMA]  // Shallow Component Test
     }).compileComponents();
   });
 
@@ -21,16 +26,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'book-rating'`, () => {
+  it(`should have as title 'Book Rating 😃'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Book Rating');
+    expect(app.title).toEqual('Book Rating 😃');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Book Rating');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Book Rating 😃');
   });
 });

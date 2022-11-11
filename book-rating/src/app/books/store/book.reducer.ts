800b5e821cc8d7a859b9a819eb1866784ad36e93
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-11-11 15:43:55 +0100

    ngrx: state

--- book-rating/src/app/books/store/book.reducer.ts
+++ book-rating/src/app/books/store/book.reducer.ts
@@ -1,14 +1,17 @@
 import { Action, createReducer, on } from '@ngrx/store';
+import { Book } from '../shared/book';
 import * as BookActions from './book.actions';
 
 export const bookFeatureKey = 'book';
 
 export interface State {
-
+  books: Book[],
+  loading: boolean
 }
 
 export const initialState: State = {
-
+  books: [],
+  loading: false
 };
 
 export const reducer = createReducer(
